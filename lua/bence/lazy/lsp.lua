0b205d40132c95827d0d9e046cd099b3243bebe5
mcsksgyrk <bbence.hajdu@gmail.com>
2024-10-01 07:15:50 +0200

    idk

--- lua/bence/lazy/lsp.lua
+++ lua/bence/lazy/lsp.lua
@@ -26,7 +26,7 @@ return {
         require("mason").setup()
         require("mason-lspconfig").setup({
             ensure_installed = {
-                "basedpyright",
+                "pylsp",
                 "julials",
         		"lemminx"
             },
